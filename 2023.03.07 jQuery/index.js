933318c58bcd702705d8a01b9be45afe252733d9
Ak1yamaKiyoshi <avoqun@gmail.com>
2023-03-08 17:30:00 +0200

    1

--- 2023.03.07 jQuery/index.js
+++ 2023.03.07 jQuery/index.js
@@ -1,9 +1,29 @@
+
 $(document).ready(function() {
-    
-	$('.currency_dropdown_toggle').click(function() {
-		$('.currency_dropdown').attr("hidden", function(index, attr) {
+    $('.currency_dropdown_toggle').click(function(event) {
+        let inOut = JSON.stringify(event.target.id)
+        if (inOut.includes("button_in")) {
+            direction = "#in";
+            direction_button = "#button_in";
+        }
+        else if (inOut.includes("button_out")) {
+            direction = "#out";
+            direction_button = "#button_out";
+        }
+        
+        
+        $(`${direction}`).attr("hidden", function(index, attr) {
             return attr == "hidden" ? null : "hidden";
         });
-        console.log('AAAAAAAAAAaa')
-	});
+
+        //console.log()
+        //console.log(event.target.innerHTML)
+        if (!JSON.stringify(event.target.innerHTML).includes("<ul")) {
+            let txt = $(`${direction_button}`).html()    
+            console.log(txt)
+            console.log()
+            console.log(txt.slice(txt.slice(txt.search('[\n]+')+1, -1).search('[\n]+'), -3));
+
+        }
+    });
 });
