6d36caf89e6e1e0a9a68c2609ca9a3c7e9bc95ff
Ak1yamaKiyoshi <avoqun@gmail.com>
2023-03-18 14:09:43 +0200

    1

--- 2023.03.07 jQuery/index.js
+++ 2023.03.07 jQuery/index.js
@@ -1,45 +1,23 @@
+let currencies= {"U.S. dollar":"USD","Euro":"EUR","Japanese yen":	"JPY","Sterling":"GBP","Renminbi":"CNY","Australian dollar":"AUD","Canadian dollar":"CAD","Swiss franc":"CHF","Hong Kong dollar":"HKD","Singapore dollar":"SGD","Swedish krona":"SEK","South Korean won":"KRW","Norwegian krone": "NOK","New Zealand dollar	": "NZD","Indian rupee": "INR","Mexican peso": "MXN","New Taiwan dollar": "TWD","South African rand": "ZAR","Brazilian real": "BRL","Danish krone": "DKK","Polish złoty": "PLN","Thai baht": "THB","Israeli new shekel": "ILS","Indonesian rupiah": "IDR","Czech koruna": "CZK","UAE dirham": "AED","Turkish lira": "TRY","Hungarian forint": "HUF","Chilean peso": "CLP","Saudi riyal": "SAR","Philippine peso": "PHP","Malaysian ringgit": "MYR","Colombian peso": "COP","Russian ruble": "RUB","Romanian leu": "RON"}
 
-$(document).ready(function() {
-    $('.currency_dropdown_toggle').click(function(event) {
-        let inOut = JSON.stringify(event.target.id)
-        if (inOut.includes("button_in")) {
-            direction = "#in";
-            direction_button = "#button_in";
-        }
-        else if (inOut.includes("button_out")) {
-            direction = "#out";
-            direction_button = "#button_out";
-        }
-
-        
-        $(`${direction}`).attr("hidden", function(index, attr) {
-            return attr == "hidden" ? null : "hidden";
+$(window).on('load', function() {
+    for (const [currency, currency_code] of Object.entries(currencies)) {
+        ['#select_currency_out', '#select_currency_in'].forEach(element => {
+            $(`${element}`).append(new Option(currency, currency_code));
         });
-
-        //console.log()
-        console.log(event.target.innerHTML)
-        if (!JSON.stringify(event.target.innerHTML).includes("<ul")) {
-            let txt = $(`${direction_button}`).html()    
-            console.log(txt)
-            console.log()
-            console.log(txt.slice(txt.slice(txt.search('[\n]+')+1, -1).search('[\n]+'), -3));
-        }
-    });
+    }
 });
 
-
-
-
-/*
-коли виникає клік, є генератор цієї події, його можна отримати $this або Jquery.this 
-$this.attr(id)
-існує метод show або hide 
-кнопки повинні відкривати чи закривати меню
-у меню до елементів відповідного класу треба теж прив'язати онклік
-$("#btnAddProfile").attr('value', 'Save');
-*/
-/*
-Переробити інпут намбером 
-
-перелік то селект 
-*/
+$(document).ready(function() {
+    $('#select_currency_out, #select_currency_in').click(function() {
+        fetch(`https://api.exchangerate-api.com/v4/latest/${$('#select_currency_in :selected').val()}`)
+        .then(res => {return res.json();})
+        .then(res => {
+            let total = res.rates[$('#select_currency_out :selected').val()] * $('#currency_in input').val();
+            if ($('#select_currency_in :selected').val() == 'RUB') total /= 1000;
+            if ($('#select_currency_out :selected').val() == 'RUB') total *= 1000;
+            $('#currency_out p').text(total);
+            console.log(    )
+        });
+    });
+});
