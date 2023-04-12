f9deb6f1f6ec4014863ad52c967efb558d2e03cf
akiyama_kiyoshi <108092986+Ak1yamaKiyoshi@users.noreply.github.com>
2023-04-12 20:37:16 +0300

    idk

---
+++ 2023.03.21 A lot of tasks/lab4/index.js
@@ -0,0 +1,13 @@
+function process() {
+    outputStringWithDifferentFont($('#str').val(), $('#fontSize').val())
+}
+
+function outputStringWithDifferentFont(str, fontSize) {
+    $('#output').css({'font-size': `${fontSize}px`});
+    $('#output').text(str);
+}
+/*
+1. Створити функцію, яка виводить текст із різним розміром шрифту. 
+Функція має два аргументи: текстовий рядок та розмір шрифту. 
+Використовуйте style-властивість font-size
+*/
