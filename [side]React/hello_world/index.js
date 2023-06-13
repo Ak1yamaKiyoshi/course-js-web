3df2634a0ed8d0c1b8ce1a3a56d0e5e6f8b4f4ae
akiyama_kiyoshi <108092986+Ak1yamaKiyoshi@users.noreply.github.com>
2023-06-13 10:13:43 +0300

    1

---
+++ [side]React/hello_world/index.js
@@ -0,0 +1,9 @@
+
+
+//  ReactDOM.render();              // allows render html-like code 
+//  <h1> Some text! </h1>           // code to render 
+//  document.getElementById('root') // Where to render code 
+ReactDOM.render(
+    <h1> Some text! </h1>, 
+    document.getElementById('root')
+);
