41c89de033df834edb264d082aeb046f3c22cd48
akiyama_kiyoshi <108092986+Ak1yamaKiyoshi@users.noreply.github.com>
2023-07-09 01:33:15 +0300

    -

--- 2023.06.20 React/next js/blog/context/IsAdminContext.js
+++
@@ -1,4 +0,0 @@
-'use client'
-import React from "react";
-
-export const IsAdminContext = React.createContext(false);
--- 2023.06.20 React/next js/blog/database/index.js
+++
@@ -1,6 +0,0 @@
-    
-const Interface_db = require('./queries')
-const db = require('./db');
-
-let inteface = new Interface_db(db);
-console.log( inteface.selectAllBlogRows)
